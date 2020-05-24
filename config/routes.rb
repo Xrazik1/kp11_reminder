869dfd14687c1f9ab3745f15e113a5e267b9dfc3
Mikola <Xrazik@yandex.ru>
2020-05-24 21:28:45 +0300

    feat: configure routing and seeds

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'groups#index'
+
+  resources :groups, only: %i[show index] do
+    resources :weeks do
+      resources :days do
+        post '/notices' => "notices#create_inline", :as => :create_inline
+      end
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+if Group.any? || Day.any?
+  puts 'seeds already loaded'
+  return
+end
+
+group1 = Group.create!(name: 'КС-42')
+group1.create_week!
+
+=begin
+day1 = week1.days.create!(name: "Понедельник", date: "21.03.2019")
+day2 = week1.days.create!(name: "Понедельник", date: "21.03.2019")
+day3 = week1.days.create!(name: "Понедельник", date: "21.03.2019")
+day4 = week1.days.create!(name: "Понедельник", date: "21.03.2019")
+day5 = week1.days.create!(name: "Понедельник", date: "21.03.2019")
+day6 = week1.days.create!(name: "Понедельник", date: "21.03.2019")
+day7 = week1.days.create!(name: "Понедельник", date: "21.03.2019")
+
+day1.notices.create!(content: '1 пара заменена на английский')
+day1.notices.create!(content: '1 пара заменена на английский')
+day1.notices.create!(content: '1 пара заменена на английский')
+
+day2.notices.create!(content: '1 пара заменена на английский')
+day2.notices.create!(content: '1 пара заменена на английский')
+day2.notices.create!(content: '1 пара заменена на английский')
+
+group2 = Group.create!(name: 'КС-31')
+week2 = group2.create_week!
+
+=end
